0be2c3c664ec8bc51624e415826c6179c524ad79
nvellon <nvellon@gmail.com>
2014-03-24 20:55:09 -0300

    - Change on directory structure. - Fix coding style.

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,4 +1,4 @@
-#include "trk/trk.hpp"
+#include "../src/trk/trk.hpp"
 
 #include <iostream>
 #include <stdio.h>
@@ -9,12 +9,12 @@ int main(int argc, char** argv)
 {
     //trk::detectFile("img/ojo.jpg");
     //trk::detectFile("img/ojo2.jpg");
-	//trk::detectFile("img/ojo3.jpg");
-	//trk::detectFile("img/ojo4.jpg");
-	
-	trk::detectCam();
+    //trk::detectFile("img/ojo3.jpg");
+    //trk::detectFile("img/ojo4.jpg");
 
-	cv::waitKey(0);
+    trk::detectCam();
 
-	return 0;
+    cv::waitKey(0);
+
+    return 0;
 }
